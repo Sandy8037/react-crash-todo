9a658334be99989e7b64b1cfcb3e59c5b8e2dde4
Sandeep Devarapalli <sandy8037@gmail.com>
2020-06-16 11:04:11 -0700

    Updates

---
+++ precache-manifest.6be58d85978667ce3a1179efdf1ad99f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51c753ea3be89e8986f086f2f6ee5e27",
+    "url": "/react-crash-todo/index.html"
+  },
+  {
+    "revision": "8e0b91093cecadd9e5d2",
+    "url": "/react-crash-todo/static/css/main.36fe29b4.chunk.css"
+  },
+  {
+    "revision": "bacedfd1e52f3ddf32cd",
+    "url": "/react-crash-todo/static/js/2.235a5a45.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-crash-todo/static/js/2.235a5a45.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e0b91093cecadd9e5d2",
+    "url": "/react-crash-todo/static/js/main.59485b4f.chunk.js"
+  },
+  {
+    "revision": "95138afc03a05e243e22",
+    "url": "/react-crash-todo/static/js/runtime-main.7cb71d26.js"
+  }
+]);
